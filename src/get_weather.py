deb498e76c91abc51776b1ff4e5b8a735d53749b
“Will <wwh22@uw.edu>
2021-01-02 19:48:40 -0800

    Added a class for the weather.

--- src/get_weather.py
+++ src/get_weather.py
@@ -5,6 +5,7 @@ to get historic/current/forcast data for the barometric pressure and humidity.
 
 from pyowm.owm import OWM
 
+
 def get_api_key(filepath):
     """Read a local file with your API key."""
     api = []
@@ -14,12 +15,26 @@ def get_api_key(filepath):
     assert len(api) == 1, "API key should be only one line long."
     return api[0]
 
+
+class LocalWeather:
+    """Class to hold the weather in user location.
+    
+    Arguments:
+    ----------
+    key: Your API key for OpeWeather
+    coords: A tuple for the lat. & lon. of your location
+    """
+    def __init__(self, key, coords):
+        self.key = key
+        self.lat, self.lon = coords
+        self.owm = OWM(self.key)
+        self.mgr = self.owm.weather_manager()
+
 # TEST: test the API works 
 # Coordinates for Seattle (lat, lon)
 coords = (47.6062,-122.3321)
 key = get_api_key("API.txt")
-owm = OWM(key)
-mgr = owm.weather_manager()
-test_dict = mgr.one_call(lat = coords[0], lon = coords[1])
-for i in test_dict.forecast_hourly:
-    print((i.reference_time('iso')))
+
+#test_dict = mgr.one_call(lat = coords[0], lon = coords[1])
+
+print(LocalWeather(key, coords).lat)
